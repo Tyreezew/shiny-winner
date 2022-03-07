accf10b5e2197f69fdcc0b25f5701becacf34534
Tyreeze <wedding5@msu.edu>
2022-03-07 10:40:49 -0600

    nodels set up complete

--- models/index.js
+++ models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
+Category.haveMany(Product, {
+  foreignKey: 'vategory_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongToMany(Tag, {
+  through: ProductTag,
+
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(Product, {
+  through: ProductTag,
+
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
